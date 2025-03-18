26fc5d2c5b44c9bfe0773639d6c167e395d18fea
Himanshu Chauhan <Himanshu Chauhan>
2025-03-19 01:38:42 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
 import HomePage from "./HomePage/Body.jsx";
-import Login from "./Login/Body.jsx";
-import Register from "./Register/Body.jsx";
-import AddProperty from "./AddProperty/Body.jsx";
-import PropertyList from "./PropertyList/Body.jsx";
-import PropertyDetails from "./PropertyDetails/Body.jsx";
+import RegistrationPage from "./RegistrationPage/Body.jsx";
+import LoginPage from "./LoginPage/Body.jsx";
+import DashboardPage from "./DashboardPage/Body.jsx";
+import AddPropertyPage from "./AddPropertyPage/Body.jsx";
+import PropertyListingPage from "./PropertyListingPage/Body.jsx";
 
 const Body = () => {
 return (
@@ -15,11 +15,11 @@ return (
       <Header />
         <Routes>
             <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/addproperty" element={<AddProperty />} />
-        <Route path="/propertylist" element={<PropertyList />} />
-        <Route path="/propertydetails" element={<PropertyDetails />} />
+        <Route path="/registrationpage" element={<RegistrationPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/dashboardpage" element={<DashboardPage />} />
+        <Route path="/addpropertypage" element={<AddPropertyPage />} />
+        <Route path="/propertylistingpage" element={<PropertyListingPage />} />
         </Routes>
       <Footer />
     </Router>
