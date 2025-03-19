b293d8d4f471859d8aca315f8c6d78e3cf4c7a5d
Himanshu Chauhan <Himanshu Chauhan>
2025-03-19 17:30:41 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
-import HomePage from "./HomePage/Body.jsx";
-import LoginPage from "./LoginPage/Body.jsx";
-import RegisterPage from "./RegisterPage/Body.jsx";
+import Home from "./Home/Body.jsx";
+import Login from "./Login/Body.jsx";
+import Register from "./Register/Body.jsx";
 import Dashboard from "./Dashboard/Body.jsx";
-import AddPropertyPage from "./AddPropertyPage/Body.jsx";
+import AddProperty from "./Add Property/Body.jsx";
 
 const Body = () => {
 return (
     <Router>
       <Header />
         <Routes>
-            <Route path="/" element={<HomePage />} />
-        <Route path="/loginpage" element={<LoginPage />} />
-        <Route path="/registerpage" element={<RegisterPage />} />
+            <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/addpropertypage" element={<AddPropertyPage />} />
+        <Route path="/addproperty" element={<AddProperty />} />
         </Routes>
       <Footer />
     </Router>
