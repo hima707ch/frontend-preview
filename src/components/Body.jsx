ae1ab117d7f41ca0b22d36617ff7d842dc795023
Himanshu Chauhan <Himanshu Chauhan>
2025-03-19 14:57:38 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
 import HomePage from "./HomePage/Body.jsx";
-import RegistrationPage from "./RegistrationPage/Body.jsx";
 import LoginPage from "./LoginPage/Body.jsx";
-import DashboardPage from "./DashboardPage/Body.jsx";
+import RegisterPage from "./RegisterPage/Body.jsx";
+import Dashboard from "./Dashboard/Body.jsx";
 import AddPropertyPage from "./AddPropertyPage/Body.jsx";
-import PropertyListingPage from "./PropertyListingPage/Body.jsx";
 
 const Body = () => {
 return (
@@ -15,11 +14,10 @@ return (
       <Header />
         <Routes>
             <Route path="/" element={<HomePage />} />
-        <Route path="/registrationpage" element={<RegistrationPage />} />
         <Route path="/loginpage" element={<LoginPage />} />
-        <Route path="/dashboardpage" element={<DashboardPage />} />
+        <Route path="/registerpage" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/addpropertypage" element={<AddPropertyPage />} />
-        <Route path="/propertylistingpage" element={<PropertyListingPage />} />
         </Routes>
       <Footer />
     </Router>
