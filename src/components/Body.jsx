bea8b4a11b00a25c775ab37820e45c90f3df994c
Himanshu Chauhan <Himanshu Chauhan>
2025-03-22 20:01:51 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,22 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
-import HomePage from "./HomePage/Body.jsx";
-import LoginPage from "./LoginPage/Body.jsx";
-import RegistrationPage from "./RegistrationPage/Body.jsx";
-import PropertiesPage from "./PropertiesPage/Body.jsx";
-import Dashboard from "./Dashboard/Body.jsx";
+import Registration from "./Registration/Body.jsx";
+import Login from "./Login/Body.jsx";
 
 const Body = () => {
 return (
     <Router>
       <Header />
         <Routes>
-            <Route path="/" element={<HomePage />} />
-        <Route path="/loginpage" element={<LoginPage />} />
-        <Route path="/registrationpage" element={<RegistrationPage />} />
-        <Route path="/propertiespage" element={<PropertiesPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
         </Routes>
       <Footer />
     </Router>
