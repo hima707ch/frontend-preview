64642c6272c31dca0b89e74220c3366fdca8f905
Himanshu Chauhan <Himanshu Chauhan>
2025-03-25 12:24:23 +0530

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,30 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
-import HomePage from "./HomePage/Body.jsx";
-import RegisterPage from "./RegisterPage/Body.jsx";
-import LoginPage from "./LoginPage/Body.jsx";
-import SellerDashboard from "./SellerDashboard/Body.jsx";
-import AddPropertyPage from "./AddPropertyPage/Body.jsx";
-import PropertyDetailsPage from "./PropertyDetailsPage/Body.jsx";
-import ProfilePage from "./ProfilePage/Body.jsx";
+import pages from "./pages/Body.jsx";
 
 const Body = () => {
-return (
-    <Router>
-      <Header />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-        <Route path="/registerpage" element={<RegisterPage />} />
-        <Route path="/loginpage" element={<LoginPage />} />
-        <Route path="/sellerdashboard" element={<SellerDashboard />} />
-        <Route path="/addpropertypage" element={<AddPropertyPage />} />
-        <Route path="/propertydetailspage" element={<PropertyDetailsPage />} />
-        <Route path="/profilepage" element={<ProfilePage />} />
-        </Routes>
-      <Footer />
-    </Router>
-);
+    return (
+        <Router>
+          <Header />
+            <Routes>
+                <Route path="/pages" element={<pages />} />
+            </Routes>
+          <Footer />
+        </Router>
+    );
 };
 
 export default Body;
