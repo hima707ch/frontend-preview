de78abdf39c845b95c65afddd7a213a1ac25f01d
root <root@9651c36be4a3>
2025-03-26 17:41:10 +0000

    Updated React components from JSON

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Global/Header.jsx";
 import Footer from "./Global/Footer.jsx";
-import pages from "./pages/Body.jsx";
+import HomePage from "./HomePage/Body.jsx";
+import SearchPage from "./SearchPage/Body.jsx";
+import PropertyAddPage from "./PropertyAddPage/Body.jsx";
+import UserProfile from "./UserProfile/Body.jsx";
 
 const Body = () => {
     return (
         <Router>
           <Header />
             <Routes>
-                <Route path="/pages" element={<pages />} />
+                <Route path="/" element={<HomePage />} />
+        <Route path="/searchpage" element={<SearchPage />} />
+        <Route path="/propertyaddpage" element={<PropertyAddPage />} />
+        <Route path="/userprofile" element={<UserProfile />} />
             </Routes>
           <Footer />
         </Router>
